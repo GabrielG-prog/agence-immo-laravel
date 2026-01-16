e29fc838229251e2c68f888119fc51f0773f986c
GabrielG <gabriyel.gmz@gmail.com>
2026-01-16 15:40:25 +0100

    partie accueil - front

---
+++ app/Http/Controllers/PropertyController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SearchPropertiesRequest;
+use App\Models\Property;
+use Illuminate\Http\Request;
+
+class PropertyController extends Controller
+{
+    public function index(SearchPropertiesRequest $request)
+    {
+        $query = Property::query()->orderBy('created_at', 'desc');
+        if($request->validated('price')) {
+            $query = $query->where('price', '<=', $request->validated('price'));
+        }
+        if($request->validated('surface')) {
+            $query = $query->where('surface', '>=', $request->validated('surface'));
+        }
+        if($request->validated('rooms')) {
+            $query = $query->where('rooms', '>=', $request->validated('rooms'));
+        }
+        if($request->validated('title')) {
+            $query = $query->where('title', 'like', "%{$request->validated('title')}%");
+        }
+        return view('home.index', [
+            'properties' => $query->paginate(16),
+            'input' => $request->validated()
+        ]);
+    }
+
+    public function show(string $slug, Property $property)
+    {
+        $expectedSlug = $property->getSlug();
+
+        if ($slug !== $expectedSlug) {
+            return to_route('property.show', [
+                'slug' => $expectedSlug,
+                'property' => $property
+            ]);
+        }
+
+        return view('home.show', [
+            'property' => $property
+        ]);
+    }
+
+}
--- app/Models/Property.php
+++ app/Models/Property.php
@@ -6,6 +6,7 @@ use App\Models\Option;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
 
 class Property extends Model
 {
@@ -28,6 +29,11 @@ class Property extends Model
     public function options(): BelongsToMany
     {
         return $this->belongsToMany(Option::class);
+    }
+
+    public function getSlug(): string
+    {
+        return Str::slug($this->title);
     }
 }
 
